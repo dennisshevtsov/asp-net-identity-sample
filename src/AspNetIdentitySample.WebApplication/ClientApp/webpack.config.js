048f4c93c59f49983580444da0c1ed14f0efff4d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-16 13:23:57 +0300

    add webpack

---
+++ src/AspNetIdentitySample.WebApplication/ClientApp/webpack.config.js
@@ -0,0 +1,24 @@
+﻿const path = require('path');
+
+module.exports = {
+  entry : './src/index.js',
+  output: {
+    clean: true,
+    filename: 'index.js',
+    path: path.resolve(__dirname, '../', 'wwwroot', 'dist'),
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use : ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|woff(2)?|tff|otf|svg)$/i,
+        use : 'asset',
+      },
+    ],
+  },
+};
