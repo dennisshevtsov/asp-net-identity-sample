e96fa712996c2b230cc4c7d3305d3aca15bc26a1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-19 12:21:44 +0300

    logout view model

---
+++ src/AspNetIdentitySample.WebApplication/ViewModels/LogoutViewModel.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.ViewModels
+{
+  /// <summary>Represents data to log out an account.</summary>
+  public sealed class LogoutViewModel
+  {
+    /// <summary>Gets/sets an object that represents a URL to that the app should return after an successful login.</summary>
+    [FromQuery]
+    public string? ReturnUrl { get; set; }
+  }
+}
