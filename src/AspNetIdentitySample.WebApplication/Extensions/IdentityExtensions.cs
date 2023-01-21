1a0a44b1a691ef13ad6ab154a84f08a3b7b4e30e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-21 16:37:36 +0300

    add comments

--- src/AspNetIdentitySample.WebApplication/Extensions/IdentityExtensions.cs
+++ src/AspNetIdentitySample.WebApplication/Extensions/IdentityExtensions.cs
@@ -12,6 +12,9 @@ namespace Microsoft.Extensions.DependencyInjection
   /// <summary>Provides a simple API to set up ASP.NET Core Identity.</summary>
   public static class IdentityExtensions
   {
+    /// <summary>Sets up services for the Identity.</summary>
+    /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
+    /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
     public static IServiceCollection SetUpIdentity(this IServiceCollection services)
     {
       services.AddIdentity<UserEntity, RoleEntity>()
