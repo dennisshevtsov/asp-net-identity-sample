ade85da5e91df0c42e4f0c27ef25fe649cb9f412
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-22 00:23:43 +0300

    add unit test for sign in controller

---
+++ test/AspNetIdentitySample.Test/Unit/SignInControllerTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.Test.Unit
+{
+  using Microsoft.AspNetCore.Identity;
+  using Moq;
+
+  using AspNetIdentitySample.ApplicationCore.Entities;
+  using AspNetIdentitySample.WebApplication.Controllers;
+
+  [TestClass]
+  public sealed class SignInControllerTest
+  {
+#pragma warning disable CS8618
+    private Mock<SignInManager<UserEntity>> _signInManagerMock;
+
+    private SignInController _signInController;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _signInManagerMock = new Mock<SignInManager<UserEntity>>();
+
+      _signInController = new SignInController(_signInManagerMock.Object);
+    }
+  }
+}
