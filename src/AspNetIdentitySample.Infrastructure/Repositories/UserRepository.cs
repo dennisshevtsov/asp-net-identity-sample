822000d16be5aaa47d465364ac59f264c1ba98ec
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-21 23:11:58 +0300

    remove unused method from user repository

--- src/AspNetIdentitySample.Infrastructure/Repositories/UserRepository.cs
+++ src/AspNetIdentitySample.Infrastructure/Repositories/UserRepository.cs
@@ -21,17 +21,6 @@ namespace AspNetIdentitySample.Infrastructure.Repositories
       _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
     }
 
-    /// <summary>Gets a user by a user ID.</summary>
-    /// <param name="userId">An object that represents an ID of a user.</param>
-    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
-    /// <returns>An object that represents an asynchronous operation that can return a value.</returns>
-    public Task<UserEntity?> GetUserAsync(Guid userId, CancellationToken cancellationToken)
-      => _dbContext.Set<UserEntity>()
-                   .AsNoTracking()
-                   .WithPartitionKey(userId.ToString())
-                   .Where(entity => entity.Id == userId)
-                   .FirstOrDefaultAsync(cancellationToken);
-
     /// <summary>Gets a user by a user email.</summary>
     /// <param name="email">An object that represents an email of a user.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
