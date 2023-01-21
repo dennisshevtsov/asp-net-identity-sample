d5304d2144b237cf51b6354a1114b99973b8269c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-21 23:23:40 +0300

    add integration test for user repository

---
+++ test/AspNetIdentitySample.Test/Integration/UserRepositoryTest.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.Test.Integration
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.DependencyInjection;
+
+  using AspNetIdentitySample.ApplicationCore.Repositories;
+
+  [TestClass]
+  public sealed class UserRepositoryTest
+  {
+    private CancellationToken _cancellationToken;
+
+#pragma warning disable CS8618
+    private IDisposable _disposable;
+
+    private IUserRepository _userRepository;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _cancellationToken = CancellationToken.None;
+
+      var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                    .Build();
+
+      var scope = new ServiceCollection().SetUpDatabase(configuration)
+                                         .BuildServiceProvider()
+                                         .CreateScope();
+
+      _disposable = scope;
+      _userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
+
+      scope.ServiceProvider.GetRequiredService<DbContext>().Database.EnsureCreated();
+
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      _disposable?.Dispose();
+    }
+  }
+}
