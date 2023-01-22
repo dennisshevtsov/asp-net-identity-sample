53a39fcb65d9e4d47ce8493fe175101586f19b39
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-23 02:00:08 +0300

    add access denied endpoint

---
+++ src/AspNetIdentitySample.WebApplication/Controllers/AccessDeniedController.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.Controllers
+{
+  using Microsoft.AspNetCore.Authorization;
+
+  [AllowAnonymous]
+  [Route("account")]
+  public sealed class AccessDeniedController : Controller
+  {
+    public const string ViewName = "AccessDeniedView";
+
+    [HttpGet("access-denied")]
+    public IActionResult Get()
+    {
+      return View(AccessDeniedController.ViewName);
+    }
+  }
+}
