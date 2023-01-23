72608e9fd6cda0fb7502b277bd51beafe25f0155
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-23 12:46:31 +0300

    add user role relation

--- src/AspNetIdentitySample.ApplicationCore/Entities/UserRoleEntity.cs
+++ src/AspNetIdentitySample.ApplicationCore/Entities/UserRoleEntity.cs
@@ -6,13 +6,13 @@ namespace AspNetIdentitySample.ApplicationCore.Entities
 {
   using AspNetIdentitySample.ApplicationCore.Identities;
 
-  /// <summary>Represents details of a role.</summary>
-  public sealed class RoleEntity : IRoleIdentity
+  /// <summary>Represents details of a user and role relation.</summary>
+  public sealed class UserRoleEntity : IUserIdentity, IRoleIdentity
   {
+    /// <summary>Gets/sets an object that represents an ID of a user.</summary>
+    public Guid UserId { get; set; }
+
     /// <summary>Gets/sets an object that represents a name of a role.</summary>
     public string? RoleName { get; set; }
-
-    /// <summary>Gets/sets an object that represents a display name of a role.</summary>
-    public string? DisplayName { get; set; }
   }
 }
