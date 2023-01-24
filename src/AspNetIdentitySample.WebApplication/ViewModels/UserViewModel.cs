9b78b24ea13c3ce8f05f45c9c5f2067ccc349ec5
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-25 00:13:00 +0300

    add base view model

---
+++ src/AspNetIdentitySample.WebApplication/ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.ViewModels
+{
+  /// <summary>Represents details of the authenticated user.</summary>
+  public sealed class UserViewModel
+  {
+    /// <summary>Gets/sets an object that represents a name of a user.</summary>
+    public string? Name { get; set; }
+
+    /// <summary>Gets/sets an object that indicates if the user is authenticated.</summary>
+    public bool IsAuthenticated { get; set; }
+  }
+}
---
+++ src/AspNetIdentitySample.WebApplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.ViewModels
+{
+  /// <summary>Represents a base view model.</summary>
+  public abstract class ViewModelBase
+  {
+    /// <summary>Gets/sets an object that represents the current user.</summary>
+    public UserViewModel User { get; set; } = new UserViewModel();
+  }
+}
