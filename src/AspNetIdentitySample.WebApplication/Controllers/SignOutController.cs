7835dda459c12e7d2163ca93d27d7bd7b1db9e14
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-25 00:51:29 +0300

    populate user for sing out view model

--- src/AspNetIdentitySample.WebApplication/Controllers/SignOutController.cs
+++ src/AspNetIdentitySample.WebApplication/Controllers/SignOutController.cs
@@ -10,6 +10,7 @@ namespace AspNetIdentitySample.WebApplication.Controllers
 
   using AspNetIdentitySample.ApplicationCore.Entities;
   using AspNetIdentitySample.WebApplication.ViewModels;
+  using AspNetIdentitySample.ApplicationCore.Repositories;
 
   /// <summary>Provides a simple API to handle HTTP requests.</summary>
   [Route("account")]
@@ -18,20 +19,28 @@ namespace AspNetIdentitySample.WebApplication.Controllers
     public const string ViewName = "SignOutView";
 
     private readonly SignInManager<UserEntity> _signInManager;
+    private readonly IUserRepository _userRepository;
 
     /// <summary>Initializes a new instance of the <see cref="AspNetIdentitySample.WebApplication.Controllers.SignOutController"/> class.</summary>
     /// <param name="signInManager">An object that provides the APIs for user sign in.</param>
-    public SignOutController(SignInManager<UserEntity> signInManager)
+    /// <param name="userRepository">An object that provides a simple API to a collection of <see cref="AspNetIdentitySample.ApplicationCore.Entities.UserEntity"/> in the database.</param>
+    public SignOutController(SignInManager<UserEntity> signInManager, IUserRepository userRepository)
     {
       _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+      _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
     }
 
     /// <summary>Handles the GET request.</summary>
     /// <param name="vm">An object that represents data to sign out an account.</param>
     /// <returns>An object that defines a contract that represents the result of an action method.</returns>
     [HttpGet("signout")]
-    public IActionResult Get(SignOutAccountViewModel vm)
+    public async Task<IActionResult> Get(SignOutAccountViewModel vm, CancellationToken cancellationToken)
     {
+      var userEntity = await _userRepository.GetUserAsync(User.Identity!.Name!, cancellationToken);
+
+      vm.User.Name = userEntity!.Name;
+      vm.User.IsAuthenticated = true;
+
       return View(SignOutController.ViewName, vm);
     }
 
