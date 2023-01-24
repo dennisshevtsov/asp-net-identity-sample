cdc105639641f28910d4e34bd7f1ee38118b5988
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-25 00:49:24 +0300

    home view model

---
+++ src/AspNetIdentitySample.WebApplication/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.ViewModels
+{
+  /// <summary>Represents the view model for the home action.</summary>
+  public sealed class HomeViewModel : ViewModelBase
+  {
+  }
+}
