04b73bc3c9313a1520e5ea28461e3a53a72c4587
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-25 23:26:35 +0300

    assign view model in model binder

--- src/AspNetIdentitySample.WebApplication/Binding/ViewModelBinder.cs
+++ src/AspNetIdentitySample.WebApplication/Binding/ViewModelBinder.cs
@@ -6,14 +6,44 @@ namespace AspNetIdentitySample.WebApplication.Binding
 {
   using Microsoft.AspNetCore.Mvc.ModelBinding;
 
+  using AspNetIdentitySample.WebApplication.ViewModels;
+  using AspNetIdentitySample.ApplicationCore.Repositories;
+
   public sealed class ViewModelBinder : IModelBinder
   {
     /// <summary>Attempts to bind a model.</summary>
     /// <param name="bindingContext">An object that represents a context that contains operating information for model binding and validation.</param>
     /// <returns>An object that represents an asynchronous operation.</returns>
-    public Task BindModelAsync(ModelBindingContext bindingContext)
+    public async Task BindModelAsync(ModelBindingContext bindingContext)
     {
-      return Task.CompletedTask;
+      var vm = (ViewModelBase)Activator.CreateInstance(bindingContext.ModelType)!;
+
+      if (bindingContext.HttpContext.User.Identity != null &&
+          bindingContext.HttpContext.User.Identity.Name != null &&
+          bindingContext.HttpContext.User.Identity.IsAuthenticated)
+      {
+        var userRepository =
+          bindingContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+        var userEntity =
+          await userRepository.GetUserAsync(
+            bindingContext.HttpContext.User.Identity.Name,
+            bindingContext.HttpContext.RequestAborted);
+
+        if (userEntity != null)
+        {
+          vm.User.Name = userEntity.Name;
+          vm.User.IsAuthenticated = true;
+        }
+      }
+
+      if (vm is SignInAccountViewModel svm)
+      {
+        svm.ReturnUrl = "/";
+        svm.Email = "test@test.test";
+        svm.Password = "test";
+      }
+
+      bindingContext.Result = ModelBindingResult.Success(vm);
     }
   }
 }
