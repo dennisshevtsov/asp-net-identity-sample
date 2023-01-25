e1828ee3f1c30525ca01ed5592266dcb8a873f83
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-26 01:05:29 +0300

    add test for view model binder test

---
+++ test/AspNetIdentitySample.Test/Unit/ViewModelBinderTest.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using AspNetIdentitySample.WebApplication.Binding;
+
+namespace AspNetIdentitySample.Test.Unit
+{
+  [TestClass]
+  public sealed class ViewModelBinderTest
+  {
+#pragma warning disable CS8618
+    private ViewModelBinder _viewModelBinder;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _viewModelBinder = new ViewModelBinder();
+    }
+  }
+}
