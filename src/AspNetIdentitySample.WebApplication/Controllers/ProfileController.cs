00d93971461c110e4f430f974694ef62caab7c69
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-27 12:49:06 +0300

    add profile controller

---
+++ src/AspNetIdentitySample.WebApplication/Controllers/ProfileController.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.Controllers
+{
+  [Route("profile")]
+  public sealed class ProfileController : Controller
+  {
+  }
+}
