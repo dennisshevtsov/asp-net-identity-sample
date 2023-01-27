db6d7f7c7adf146d0e8e2b750aa30ffa9cb29dca
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-27 23:10:33 +0300

    add return url for profile

--- src/AspNetIdentitySample.WebApplication/ViewModels/ProfileViewModel.cs
+++ src/AspNetIdentitySample.WebApplication/ViewModels/ProfileViewModel.cs
@@ -15,6 +15,10 @@ namespace AspNetIdentitySample.WebApplication.ViewModels
     /// <summary>Gets/sets an object that represents an email of a user.</summary>
     public string? Email { get; set; }
 
+    /// <summary>Gets/sets an object that represents a return URL.</summary>
+    [FromQuery]
+    public string ReturnUrl { get; set; } = "/";
+
     /// <summary>Populates the view model with the <see cref="AspNetIdentitySample.ApplicationCore.Entities.UserEntity"/>.</summary>
     /// <param name="userEntity">An object that represents details of a user.</param>
     public void FromEntity(UserEntity userEntity)
