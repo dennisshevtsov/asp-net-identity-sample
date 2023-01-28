270273d22bd1ce615314fb33a50f71289f657d2a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-29 01:19:30 +0300

    implement user list view model

--- src/AspNetIdentitySample.WebApplication/ViewModels/UserListViewModel.cs
+++ src/AspNetIdentitySample.WebApplication/ViewModels/UserListViewModel.cs
@@ -4,8 +4,25 @@
 
 namespace AspNetIdentitySample.WebApplication.ViewModels
 {
+  using AspNetIdentitySample.ApplicationCore.Identities;
+
   /// <summary>Represents the view model for the user list action.</summary>
   public sealed class UserListViewModel : ViewModelBase
   {
+    /// <summary>Gets/sets an object that represents a collection of user records.</summary>
+    public IList<UserViewModel> Users { get; set; } = new List<UserViewModel>();
+
+    /// <summary>Represents the view model for the user record.</summary>
+    public sealed class UserViewModel : IUserIdentity
+    {
+      /// <summary>Gets/sets an object that represents an ID of a user.</summary>
+      public Guid UserId { get; set; }
+
+      /// <summary>Gets/sets an object that represents a name of a user.</summary>
+      public string? Name { get; set; }
+
+      /// <summary>Gets/sets an object that represents an email of a user.</summary>
+      public string? Email { get; set; }
+    }
   }
 }
