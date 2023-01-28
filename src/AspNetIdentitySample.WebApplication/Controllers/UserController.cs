edf56137c09ce34f7ecf279dbbcf9499d4875ecc
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-29 01:11:35 +0300

    user controller

---
+++ src/AspNetIdentitySample.WebApplication/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.Controllers
+{
+  using AspNetIdentitySample.WebApplication.ViewModels;
+
+  /// <summary>Provides a simple API to handle HTTP requests.</summary>
+  [Route("user")]
+  public class UserController : Controller
+  {
+    public const string ViewName = "UserView";
+
+    [HttpGet]
+    public IActionResult Get(UserListViewModel vm)
+    {
+      return View(UserController.ViewName, vm);
+    }
+  }
+}
