cb525cf0721e948a9b37e165d533058b47aca1a6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-30 12:22:16 +0300

    add routing defaultsg

---
+++ src/AspNetIdentitySample.WebApplication/Defaults/Routing.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.Defaults
+{
+  public static class Routing
+  {
+    public const string UserRoute = "user";
+
+    public const string AccountRoute = "account";
+
+    public const string AccessDeniedEndpoint = "access-denied";
+
+    public const string SignInEndpoint = "signin";
+
+    public const string SignOutEndpoint = "signout";
+  }
+}
