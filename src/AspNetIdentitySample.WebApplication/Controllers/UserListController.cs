20ae51fd43621ebf5497bf604b64ce62ac3be72a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-30 12:01:15 +0300

    add comments

--- src/AspNetIdentitySample.WebApplication/Controllers/UserListController.cs
+++ src/AspNetIdentitySample.WebApplication/Controllers/UserListController.cs
@@ -17,11 +17,17 @@ namespace AspNetIdentitySample.WebApplication.Controllers
 
     private readonly IUserService _userService;
 
+    /// <summary>Initializes a new instance of the <see cref="AspNetIdentitySample.WebApplication.Controllers.UserListController"/> class.</summary>
+    /// <param name="userService">An object that provides a simple API to execute queries and commands with the <see cref="AspNetIdentitySample.ApplicationCore.Entities.UserEntity"/>.</param>
     public UserListController(IUserService userService)
     {
       _userService = userService ?? throw new ArgumentNullException(nameof(userService));
     }
 
+    /// <summary>Handles the GET request.</summary>
+    /// <param name="vm">An object that represents the view model for the user list action.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation that can return a value.</returns>
     [HttpGet]
     public async Task<IActionResult> Get(UserListViewModel vm, CancellationToken cancellationToken)
     {
