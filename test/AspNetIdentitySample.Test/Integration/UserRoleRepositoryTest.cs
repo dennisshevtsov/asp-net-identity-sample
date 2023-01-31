17048286c6db91b90d90e001eefcefaa48d94567
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-31 22:02:37 +0300

    add integration test for user role repository

---
+++ test/AspNetIdentitySample.Test/Integration/UserRoleRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.Test.Integration
+{
+  using Microsoft.Extensions.DependencyInjection;
+
+  using AspNetIdentitySample.ApplicationCore.Repositories;
+
+  [TestClass]
+  public sealed class UserRoleRepositoryTest : IntegrationTestBase
+  {
+#pragma warning disable CS8618
+    private IUserRoleRepository _userRoleRepository;
+#pragma warning restore CS8618
+
+    protected override void InitializeInternal()
+    {
+      _userRoleRepository = Scope.ServiceProvider.GetRequiredService<IUserRoleRepository>();
+    }
+  }
+}
