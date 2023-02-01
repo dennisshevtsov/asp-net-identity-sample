c2bae75ebecde87b5666ac94d28f878323fc7de1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-01 13:45:41 +0300

    add unit test for user list controller

--- test/AspNetIdentitySample.Test/Unit/Controllers/UserListControllerTest.cs
+++ test/AspNetIdentitySample.Test/Unit/Controllers/UserListControllerTest.cs
@@ -4,8 +4,11 @@
 
 namespace AspNetIdentitySample.Test.Unit.Controllers
 {
+  using Microsoft.AspNetCore.Mvc;
+
   using AspNetIdentitySample.ApplicationCore.Services;
   using AspNetIdentitySample.WebApplication.Controllers;
+  using AspNetIdentitySample.WebApplication.ViewModels;
 
   [TestClass]
   public sealed class UserListControllerTest
@@ -24,6 +27,49 @@ namespace AspNetIdentitySample.Test.Unit.Controllers
       _userServiceMock = new Mock<IUserService>();
       _userListController = new UserListController(_userServiceMock.Object);
       _cancellationToken = CancellationToken.None;
+    }
+
+    [TestMethod]
+    public async Task Get_Should_Return_View_Result_With_View_Model()
+    {
+      var userId = Guid.NewGuid();
+      var userEntityCollection = new List<UserEntity>
+      {
+        new UserEntity
+        {
+          Id = userId,
+          UserId = userId,
+          Email = Guid.NewGuid().ToString(),
+          Name = Guid.NewGuid().ToString(),
+        },
+      };
+
+      _userServiceMock.Setup(service => service.GetUsersAsync(It.IsAny<CancellationToken>()))
+                      .ReturnsAsync(userEntityCollection)
+                      .Verifiable();
+
+      var vm = new UserListViewModel();
+
+      var actionResult = await _userListController.Get(vm, _cancellationToken);
+
+      Assert.IsNotNull(actionResult);
+
+      var viewResult = actionResult as ViewResult;
+
+      Assert.IsNotNull(viewResult);
+      Assert.AreEqual(UserListController.ViewName, viewResult.ViewName);
+
+      var model = viewResult.Model as UserListViewModel;
+
+      Assert.IsNotNull(model);
+      Assert.AreEqual(userEntityCollection.Count, model.Users.Count);
+
+      Assert.AreEqual(userEntityCollection[0].UserId, model.Users[0].UserId);
+      Assert.AreEqual(userEntityCollection[0].Email, model.Users[0].Email);
+      Assert.AreEqual(userEntityCollection[0].Name, model.Users[0].Name);
+
+      _userServiceMock.Verify(service => service.GetUsersAsync(_cancellationToken));
+      _userServiceMock.VerifyNoOtherCalls();
     }
   }
 }
