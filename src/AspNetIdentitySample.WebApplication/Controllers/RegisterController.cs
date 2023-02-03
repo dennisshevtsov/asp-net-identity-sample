8c63d7ed757fca13ab78dd8ddfdb8aec5d75da3b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-03 18:08:43 +0300

    add action for register user page

--- src/AspNetIdentitySample.WebApplication/Controllers/RegisterController.cs
+++ src/AspNetIdentitySample.WebApplication/Controllers/RegisterController.cs
@@ -5,10 +5,23 @@
 namespace AspNetIdentitySample.WebApplication.Controllers
 {
   using AspNetIdentitySample.WebApplication.Defaults;
+  using AspNetIdentitySample.WebApplication.ViewModels;
 
   /// <summary>Provides a simple API to handle HTTP requests.</summary>
   [Route(Routing.AccountRoute)]
   public sealed class RegisterController : Controller
   {
+    public const string ViewName = "RegisterView";
+
+    /// <summary>Handles the GET request.</summary>
+    /// <param name="vm">An object that represents data to register a new user.</param>
+    /// <returns>An object that defines a contract that represents the result of an action method.</returns>
+    [HttpGet(Routing.RegisterEndpoint)]
+    public IActionResult Get(RegisterUserViewModel vm)
+    {
+      ModelState.Clear();
+
+      return View(RegisterController.ViewName, vm);
+    }
   }
 }
