f9eb98f1e5c5c8edf3c1509dd856edf245c5f4e8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-03 17:55:29 +0300

    add account controller

---
+++ src/AspNetIdentitySample.WebApplication/Controllers/RegisterController.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.Controllers
+{
+  using AspNetIdentitySample.WebApplication.Defaults;
+
+  /// <summary>Provides a simple API to handle HTTP requests.</summary>
+  [Route(Routing.AccountRoute)]
+  public sealed class RegisterController : Controller
+  {
+  }
+}
