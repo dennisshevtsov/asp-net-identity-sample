2013f8ab633b57bdc4dbfaaee46618ed0fb879ad
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-07 16:07:48 +0300

    add unobtrusive validation

--- src/AspNetIdentitySample.WebApplication/ClientApp/src/js/index.js
+++ src/AspNetIdentitySample.WebApplication/ClientApp/src/js/index.js
@@ -4,3 +4,5 @@ import 'bootstrap/scss/bootstrap.scss';
 import '../scss/index.scss';
 
 import 'jquery';
+import 'jquery-validation/dist/jquery.validate';
+import 'jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive';
