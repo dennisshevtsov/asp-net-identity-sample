5097a53fb77d25fdbc9676a2f8cf1fdfb89f5302
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-08 13:18:54 +0300

    add unit test for user identity mapping

---
+++ test/AspNetIdentitySample.Test/Unit/Mapping/UserIdentityMappingTest.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetIdentitySample.WebApplication.Mapping.Test
+{
+  using System.Security.Claims;
+
+  using AutoMapper;
+  using Microsoft.Extensions.DependencyInjection;
+
+  using AspNetIdentitySample.WebApplication.ViewModels;
+
+  [TestClass]
+  public sealed class UserIdentityMappingTest
+  {
+#pragma warning disable CS8618
+    private IDisposable _disposable;
+
+    private IMapper _mapper;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      var scope = new ServiceCollection().SetUpMapping()
+                                         .BuildServiceProvider()
+                                         .CreateAsyncScope();
+
+      _disposable = scope;
+
+      _mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      _disposable?.Dispose();
+    }
+
+    [TestMethod]
+    public void Map_Should_Map_View_Model_To_Principal()
+    {
+      var userId = Guid.NewGuid();
+      var viewModel = new CurrentAccountViewModel
+      {
+        UserId = userId,
+      };
+
+      var principal = _mapper.Map<ClaimsPrincipal>(viewModel);
+
+      Assert.IsNotNull(principal);
+
+      var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
+      
+      Assert.IsNotNull(claim);
+      Assert.AreEqual(userId.ToString(), claim.Value);
+    }
+  }
+}
+
